1ccd12b0940d5031587259bb8f7b007318123ebd
jilongcui <jilongcui@163.com>
2022-07-14 20:18:07 +0800

    Fixed local strategy error.

--- src/modules/system/auth/strategies/local.strategy.ts
+++ src/modules/system/auth/strategies/local.strategy.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { ReqLoginDto } from 'src/modules/login/dto/req-login.dto';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(
+    private readonly authService: AuthService,
   ) {
     super(
       {
