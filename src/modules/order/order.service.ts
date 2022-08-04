2b63b653131e8778d93b466b3b0e673b31922264
jilongcui <jilongcui@163.com>
2022-08-04 15:45:09 +0800

    Optimize order count redis lock.

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -2,8 +2,6 @@ import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
 import { Injectable, Logger, ParseArrayPipe } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as moment from 'moment';
-import Redlock from 'redlock';
-// import { RedisLockService } from 'nestjs-simple-redis-lock';
 import { COLLECTION_ORDER_COUNT, ACTIVITY_ORDER_TEMPLATE_KEY, COLLECTION_ORDER_SUPPLY } from 'src/common/contants/redis.contant';
 import { PaginatedDto } from 'src/common/dto/paginated.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
@@ -16,34 +14,11 @@ import { Order } from './entities/order.entity';
 @Injectable()
 export class OrderService {
   logger = new Logger(OrderService.name)
-  redLock: Redlock;
   constructor(
     @InjectRepository(Order) private readonly orderRepository: Repository<Order>,
     @InjectRepository(Activity) private readonly activityRepository: Repository<Activity>,
     @InjectRedis() private readonly redis: Redis,
-    // protected readonly lockService: RedisLockService, // inject RedisLockService
   ) {
-    this.redLock = new Redlock([redis], {
-      // The expected clock drift; for more details see:
-      // http://redis.io/topics/distlock
-      driftFactor: 0.01, // multiplied by lock ttl to determine drift time
-
-      // The max number of times Redlock will attempt to lock a resource
-      // before erroring.
-      retryCount: 10,
-
-      // the time in ms between attempts
-      retryDelay: 200, // time in ms
-
-      // the max time in ms randomly added to retries
-      // to improve performance under high contention
-      // see https://www.awsarchitectureblog.com/2015/03/backoff.html
-      retryJitter: 200, // time in ms
-
-      // The minimum remaining time on a lock before an extension is automatically
-      // attempted with the `using` API.
-      automaticExtensionThreshold: 500, // time in ms
-    });
   }
   async create(createOrderDto: CreateOrderDto, userId: number) {
     // 开启事务
@@ -52,16 +27,18 @@ export class OrderService {
     // 否则，就失败
     let activity: Activity
     let orderCount: any
-    let countKey = `${COLLECTION_ORDER_COUNT}${createOrderDto.activityId}`;
+    let countKey = COLLECTION_ORDER_COUNT + ":" + createOrderDto.activityId;
 
-    await this.redLock.using([countKey], 5000,
-      async (signal) => {
-        orderCount = await this.redis.get(`${COLLECTION_ORDER_COUNT}:${createOrderDto.activityId}`)
-        if (orderCount <= 0) {
-          throw new ApiException('已售完')
-        }
-        await this.redis.decr(`${COLLECTION_ORDER_COUNT}:${createOrderDto.activityId}`)
-      });
+    do {
+      const [execError] = await this.redis.multi().decr(countKey).exec()
+      orderCount = execError[1]
+      if (orderCount < 0) {
+        // await this.redis.unwatch()
+        throw new ApiException('已售完')
+      }
+      // this.logger.log(execError[0])
+      this.logger.log(orderCount)
+    } while (0)
 
     return await getManager().transaction(async manager => {
 
@@ -79,11 +56,11 @@ export class OrderService {
       order.invalidTime = moment(moment.now()).add(5, 'minute').toDate()
       order.collections = activity.collections;
       await manager.save(order);
-      orderCount--;
-      if (orderCount % 100 === 0) {
-        activity.current = activity.current + 1;
-        await manager.save(activity)
-      }
+      // orderCount--;
+      // if (orderCount % 100 === 0) {
+      //   activity.current = activity.current + 1;
+      //   await manager.save(activity)
+      // }
       return order;
     });
 
