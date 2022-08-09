94ff331dae4834ff6c11116e39e88b2bbbad1c67
jilongcui <jilongcui@163.com>
2022-08-09 12:04:03 +0800

    Fix the error.

--- src/modules/activity/dto/request-activity.dto.ts
+++ src/modules/activity/dto/request-activity.dto.ts
@@ -2,5 +2,6 @@ import { OmitType, PartialType } from "@nestjs/swagger";
 import { Activity } from "../entities/activity.entity";
 
 export class CreateActivityDto extends OmitType(Activity, ['id', 'status'] as const) { }
+export class UpdateAllActivityDto extends Activity { }
 export class UpdateActivityDto extends PartialType(Activity) { }
 export class ListActivityDto extends PartialType(Activity) { }
