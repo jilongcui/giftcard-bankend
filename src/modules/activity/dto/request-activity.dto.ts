023dab0ebeb156b2eff20e8e2ea5b1f3d244cba1
jilongcui <jilongcui@163.com>
2022-08-09 11:55:15 +0800

    Remove status field from CreateActivityDto.

--- src/modules/activity/dto/request-activity.dto.ts
+++ src/modules/activity/dto/request-activity.dto.ts
@@ -1,7 +1,6 @@
 import { OmitType, PartialType } from "@nestjs/swagger";
 import { Activity } from "../entities/activity.entity";
 
-export class CreateActivityDto extends OmitType(Activity, ['id'] as const) { }
-export class UpdateAllActivityDto extends Activity { }
+export class CreateActivityDto extends OmitType(Activity, ['id', 'status'] as const) { }
 export class UpdateActivityDto extends PartialType(Activity) { }
 export class ListActivityDto extends PartialType(Activity) { }
