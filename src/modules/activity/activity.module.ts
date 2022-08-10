5f4ccd9cc5eac6bb21717387b14bcb7d1a7b3891
jilongcui <jilongcui@163.com>
2022-08-10 15:25:43 +0800

    Fixed error.

--- src/modules/activity/activity.module.ts
+++ src/modules/activity/activity.module.ts
@@ -4,11 +4,11 @@ import { ActivityController } from './activity.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Collection } from '../collection/entities/collection.entity';
 import { Activity } from './entities/activity.entity';
-import { Preemption } from '../assistant/preemption/entities/preemptionWhitelist.entity';
+import { PreemptionWhitelist } from '../assistant/preemption/entities/preemptionWhitelist.entity';
 import { PreemptionActivity } from '../assistant/preemption/entities/preemption-activity.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Activity, Preemption, PreemptionActivity, Collection])],
+  imports: [TypeOrmModule.forFeature([Activity, PreemptionWhitelist, PreemptionActivity, Collection])],
   controllers: [ActivityController],
   providers: [ActivityService]
 })
