f96773eaa37501f93b8a6f385293d2a4e80d68e6
jilongcui <jilongcui@163.com>
2022-08-10 15:22:14 +0800

    Fixed error of preemption.

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -8,11 +8,11 @@ import { Account } from '../account/entities/account.entity';
 import { Asset } from '../collection/entities/asset.entity';
 import { Collection } from '../collection/entities/collection.entity';
 import { AssetRecord } from '../market/entities/asset-record.entity';
-import { Preemption } from '../assistant/preemption/entities/preemptionWhitelist.entity';
+import { PreemptionWhitelist } from '../assistant/preemption/entities/preemptionWhitelist.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order, Activity, Account, Asset, Collection, AssetRecord, Preemption]),
+    TypeOrmModule.forFeature([Order, Activity, Account, Asset, Collection, AssetRecord, PreemptionWhitelist]),
   ],
   controllers: [OrderController],
   providers: [OrderService]
