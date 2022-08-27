ff805e7d7f21638ab0ea91563dc1f7ff9dc4521d
jilongcui <jilongcui@163.com>
2022-08-28 07:19:56 +0800

    Remove invite user parent limit.

--- libs/modules/src/inviteuser/invite-user.service.ts
+++ libs/modules/src/inviteuser/invite-user.service.ts
@@ -14,15 +14,15 @@ export class InviteUserService {
     async bindParent(user: User, parentId: number) {
         let inviteUser = await this.inviteUserRepository.findOneBy({ userId: user.userId })
         const parent = await this.inviteUserRepository.findOneBy({ userId: parentId })
-        if (!parent) {
-            if (parentId !== 1) {
-                throw new ApiException('父类没被邀请')
-            }
-            const initParent = new InviteUser()
-            initParent.userId = parentId
-            initParent.userName = 'admin'
-            await this.inviteUserRepository.save(initParent)
-        }
+        // if (!parent) {
+        // if (parentId !== 1) {
+        //     throw new ApiException('父类没被邀请')
+        // }
+        // const initParent = new InviteUser()
+        // initParent.userId = parentId
+        // initParent.userName = 'admin'
+        // await this.inviteUserRepository.save(initParent)
+        // }
         if (inviteUser) {
             inviteUser.parent = parent
         } else {
