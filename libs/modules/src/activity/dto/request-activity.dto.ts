235a050a399db8680dc0ac90c6ba151715619fd3
jilongcui <jilongcui@163.com>
2022-09-30 17:49:32 +0800

    Optimize CreateActivityDto.

--- libs/modules/src/activity/dto/request-activity.dto.ts
+++ libs/modules/src/activity/dto/request-activity.dto.ts
@@ -3,14 +3,34 @@ import { OmitType, PartialType, PickType } from "@nestjs/swagger";
 import { Activity } from "../entities/activity.entity";
 
 
-export class CreateActivityDto extends OmitType(Activity, ['id', 'top', 'supply', 'avatar', 'authorName', 'status', 'preemption'] as const) {
+// export class CreateActivityDto extends OmitType(Activity, ['id', 'top', 'supply', 'avatar', 'authorName', 'status', 'preemption'] as const) {
+//     preemption?: {
+//         activityId: number,
+//         desc: string,
+//         limit: number,
+//         startTime: Date
+//     }
+//     collections?: Collection[]
+// }
+type CollectionId = {
+    id: number
+}
+export class CreateActivityDto {
     preemption?: {
         activityId: number,
         desc: string,
         limit: number,
         startTime: Date
     }
-    collections?: Collection[]
+    collections: CollectionId[]
+    type: string
+    title: string
+    ruleInfo: string
+    startTime: Date
+    coverImage: string
+    endTime: Date
+    current: number
+    price: number
 }
 export class UpdateAllActivityDto extends Activity { }
 export class UpdateActivityDto extends PartialType(Activity) { }
