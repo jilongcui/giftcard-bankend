d89e8c7d7d15d363257a45f39feb59b77217e16e
jilongcui <jilongcui@163.com>
2023-03-13 21:49:52 +0800

    Fixed error.

--- libs/common/src/guards/weixin-auth.guard.ts
+++ libs/common/src/guards/weixin-auth.guard.ts
@@ -33,7 +33,7 @@ export class WeixinAuthGuard extends AuthGuard('weixin') {
         if (err || !user) {
             const request = this.context.switchToHttp().getRequest()
             request.user = user
-            request.body.userName = user.userName
+            request.body.username = user.userName
             this.logService.addLogininfor(request, err.response)
             throw err || new ApiException(err);
         }
