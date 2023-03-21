3f0e70b1ae90d6b46ffc094bbe0b8d8978f8443b
jilongcui <jilongcui@163.com>
2023-03-21 16:36:44 +0800

    Add email module for aws SES service.

--- libs/modules/src/email/email.module.ts
+++ libs/modules/src/email/email.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
+import { User } from '@app/common/decorators/user.decorator';
+import { Email } from './entities/email.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Email, User]),
+  ],
   controllers: [EmailController],
   providers: [EmailService],
   exports: [EmailService]
