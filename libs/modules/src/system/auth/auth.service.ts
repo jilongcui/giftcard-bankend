46e85c8f41fc3037efdafbbb4468aaa34cc3d404
jilongcui <jilongcui@163.com>
2023-03-29 17:56:56 +0800

    Add unionId to user model.

--- libs/modules/src/system/auth/auth.service.ts
+++ libs/modules/src/system/auth/auth.service.ts
@@ -73,8 +73,13 @@ export class AuthService {
       throw new ApiException(info.data.errmsg)
     }
 
-    // 通过openid 来查找用户是否存在
-    const user = await this.userService.findOneByOpenId(info.data.openid)
+    let user = await this.userService.findOneByUnionId(info.data.unionid)
+
+    if(!user) {
+      // 通过openid 来查找用户是否存在
+      user = await this.userService.findOneByOpenId(info.data.openid)
+    }
+
     if (!user) {
       /* 如果用户不存在，需要创建新的用户 */
 
@@ -87,10 +92,13 @@ export class AuthService {
       reqAddUserDto.postIds = [];
       reqAddUserDto.roleIds = [];
       reqAddUserDto.openId = info.data.openid;
-
+      reqAddUserDto.unionId = info.data.unionid;
       reqAddUserDto.createBy = reqAddUserDto.updateBy = 'admin'
       return await this.userService.addUser(reqAddUserDto)
-
+    }
+    if (!user.unionId) {
+      user.unionId = info.data.unionid;
+      await this.userService.changeUnionId(user.userId, info.data.unionid)
     }
     return user
   }
@@ -107,8 +115,14 @@ export class AuthService {
       throw new ApiException(info.data.errmsg)
     }
 
-    // 通过openid 来查找用户是否存在
-    const user = await this.userService.findOneByOpenId(info.data.openid)
+    // 通过unionId来查找用户是否存在
+    let user = await this.userService.findOneByUnionId(info.data.unionid)
+
+    if(!user) {
+      // 通过openid 来查找用户是否存在
+      user = await this.userService.findOneByOpenId(info.data.openid)
+    }
+    
     if (!user) {
       /* 如果用户不存在，需要创建新的用户 */
       const reqAddUserDto = new ReqAddUserDto()
@@ -120,10 +134,15 @@ export class AuthService {
       reqAddUserDto.postIds = [];
       reqAddUserDto.roleIds = [];
       reqAddUserDto.openId = info.data.openid;
+      reqAddUserDto.unionId = info.data.unionid;
 
       reqAddUserDto.createBy = reqAddUserDto.updateBy = 'admin'
       return await this.userService.addUser(reqAddUserDto)
 
+    }
+    if (!user.unionId) {
+      user.unionId = info.data.unionid;
+      await this.userService.changeUnionId(user.userId, info.data.unionid)
     }
     return user
   }
