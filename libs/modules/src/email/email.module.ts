30434f5c6f44b7465c18e8fab9f51fb28583267b
jilongcui <jilongcui@163.com>
2023-03-30 16:49:15 +0800

    Fixed email module error.

--- libs/modules/src/email/email.module.ts
+++ libs/modules/src/email/email.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
-import { User } from '@app/common/decorators/user.decorator';
 import { Email } from './entities/email.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../system/user/entities/user.entity';
 
 @Module({
   imports: [
