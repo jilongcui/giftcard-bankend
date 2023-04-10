60f0ce1e69e2d65577f49ce92cebf61a58b28080
jilongcui <jilongcui@163.com>
2023-04-10 15:00:18 +0800

    Fixed miss module.

--- libs/modules/src/account/account.module.ts
+++ libs/modules/src/account/account.module.ts
@@ -3,9 +3,10 @@ import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
+import { Currency } from '../currency/entities/currency.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account, Currency])],
   controllers: [AccountController],
   providers: [AccountService]
 })
