bc50ab752c91064045938a6d4bd4660b0350bf7e
jilongcui <jilongcui@163.com>
2023-04-11 11:10:15 +0800

    Add upgrade bankcard.

--- libs/modules/src/bankcard/bankcard.module.ts
+++ libs/modules/src/bankcard/bankcard.module.ts
@@ -8,9 +8,10 @@ import { KycModule } from '../kyc/kyc.module';
 import { Kyc } from '../kyc/entities/kyc.entity';
 import { Cardinfo } from '../cardinfo/entities/cardinfo.entity';
 import { User } from '../system/user/entities/user.entity';
+import { CardinfoModule } from '../cardinfo/cardinfo.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bankcard, Kyc, Cardinfo, User]), IdentityModule, KycModule],
+  imports: [TypeOrmModule.forFeature([Bankcard, Kyc, Cardinfo, User]), IdentityModule, KycModule, CardinfoModule],
   controllers: [BankcardController],
   providers: [BankcardService],
   exports: [BankcardService]
