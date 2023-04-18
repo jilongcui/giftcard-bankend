34b1ec0b75990732fd36fdcbc556e0366f1a1287
jilongcui <jilongcui@163.com>
2023-04-18 17:11:03 +0800

    Fixed transfer module.

--- libs/modules/src/transfer/transfer.module.ts
+++ libs/modules/src/transfer/transfer.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TransferService } from './transfer.service';
 import { TransferController } from './transfer.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from '../account/entities/account.entity';
+import { Currency } from '../currency/entities/currency.entity';
+import { Exchange } from '../exchange/entities/exchange.entity';
+import { UserModule } from '../system/user/user.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Account, Currency, Exchange]), UserModule],
   controllers: [TransferController],
-  providers: [TransferService]
+  providers: [TransferService],
+  exports: [TransferService]
 })
 export class TransferModule {}
