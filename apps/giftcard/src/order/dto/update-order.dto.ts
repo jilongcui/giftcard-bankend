0b7b2e17ddf891d8cbe75dd568e33dd9d5dc0c31
jilongcui <jilongcui@163.com>
2023-05-11 23:21:58 +0800

    Fixed order update homeaddress.

--- apps/giftcard/src/order/dto/update-order.dto.ts
+++ apps/giftcard/src/order/dto/update-order.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateOrderDto } from './create-order.dto';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
 export class UpdateOrderStatusDto {
@@ -9,8 +9,17 @@ export class UpdateOrderStatusDto {
 }
 
 export class UpdateOrderHomeAddressDto {
+    @IsOptional()
     @IsString()
-    homeAddress: string
+    homeAddress?: string
+
+    @IsString()
+    @IsOptional()
+    userName?: string
+
+    @IsString()
+    @IsOptional()
+    userPhone?: string
 }
 
 export class UpdateOrderShipDto {
