37d50df8546f01ebf29d262bc9eddbb11c6c38b6
jilongcui <jilongcui@163.com>
2023-07-17 11:16:57 +0800

    Add user filter for withdraw list.

--- libs/modules/src/wallet/collect/dto/req-rechargecollect-list.dto.ts
+++ libs/modules/src/wallet/collect/dto/req-rechargecollect-list.dto.ts
@@ -3,6 +3,7 @@ import { IsNumber, IsOptional, IsString } from "class-validator";
 import { PaginationDto } from "@app/common/dto/pagination.dto";
 import { RechargeCollect } from "../entities/rechage-collect.entity";
 import { AddressTypeEnum } from "../../address/entities/address.entity";
+import { Type } from "class-transformer";
 
 export class ReqAddRechargeCollectDto extends OmitType(RechargeCollect, ['id', 'createTime'] as const) { }
 export class ReqCollectRechargeNotifyDto extends OmitType(RechargeCollect, ['id', 'state', 'confirmState', 'feeState', 'createTime', 'userId', 'fee'] as const) { }
@@ -11,6 +12,7 @@ export class ListRechargeCollectDto {
 
     /* 用户Id */
     @IsOptional()
+    @Type()
     @IsNumber()
     userId?: number;
 
